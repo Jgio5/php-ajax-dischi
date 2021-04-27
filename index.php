cb3042bf4d4dd73124cd3ca7776edfbf2ec5d093
Jorge Fabrizio Lopez Quiroz <giorgiofabrilopez@gmail.com>
2021-04-28 00:43:03 +0200

    Milestone 1

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+    include  __DIR__ .'/partials/db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Css -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>Lista Dischi</title>
+</head>
+<body>
+<body>
+    <!-- Header -->
+    <?php
+        include  __DIR__ .'/partials/header.php';
+    ?>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <main>
+        <div class="container">
+
+            <!-- song -->
+            <div id ="section_cd">
+                
+                    <?php foreach ($database as $song) {?>
+
+                        <div class="box">
+                            <div class="album_song">
+                                <img src="<?php echo $song['poster'] ?>" alt="Album song">
+                            </div>
+
+                            <div class="title_song">
+                                <?php echo "<h1>" . $song['title'] ."</h1>" ?>
+                            </div>
+
+                            <div class="artist_age">
+                                <?php echo "<p>" . $song['author'] ."</p>"; ?>
+                                <?php echo "<p>" . $song['year'] ."</p>"; ?>
+                            </div> 
+                        </div> 
+
+                    <?php } ?>
+            </div>
+            <!-- /song -->
+        </div>
+    </main>
+    <!-- Main -->
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="logo">
+        <img src="img/spotify_logo.png" alt="logo spotify">
+    </div>
+</header>
